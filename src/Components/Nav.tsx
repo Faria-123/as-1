d19cd08019d419d1c4a35a1431b0c7cbaf64580d
Faria-123 <mrsnoushin1987@gmail.com>
2026-09-12 03:00:25 +0600

    here bugs resolved

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import logo from '../assets/logo-text.png';
-// const Nav = () => {
-//     return (
-//         <nav className=' bg-white shadow-md px-4 py-7'>
-//             <div className='container mx-auto flex justify-between items-center'>
-//                 <div>
-//                     <img src={logo} alt="logo" />
-
-//                 </div>
-//                 <div>
-//                     <ul className='flex gap-4'>
-//                         <li className='text-pink-500'>Home</li>
-//                         <li>Technologies</li>
-//                         <li>Projects</li>
-//                         <li>About</li>
-//                         <li>Contact</li>
-//                     </ul>
-//                 </div>
-//                 <div className='flex gap-4'>
-//                     <button >Sign In</button>
-//                     <button className='nav-btn'>Sign Up</button>
-//                 </div>
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default Nav;
 
 import { useState } from 'react';
 import logo from '../assets/logo-text.png';
